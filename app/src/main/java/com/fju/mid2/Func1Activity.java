8b44bac43b0d08f564b4e27cf90f4ea222029895
Betty Jang <bettyjang1005@gmail.com>
2017-12-12 01:23:15 +0800

    Home Work

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,18 +1,24 @@
 package com.fju.mid2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
-
+    private TextView roll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
     }
-    public void login (View view){
-      setResult(RESULT_OK,getIntent());
-        finish();
-    }
+   public void dice(View view){
+        int d = new Random().nextInt(6)+1;
+        new AlertDialog.Builder(this).setTitle("Roll the Dice")
+                .setMessage("Number"+d).setPositiveButton("OK",null)
+                .show();
+   }
 }
--- app/src/main/java/com/fju/mid2/MainActivity.java
+++ app/src/main/java/com/fju/mid2/MainActivity.java
@@ -1,44 +1,82 @@
 package com.fju.mid2;
 
+
+
 import android.content.Intent;
+import android.icu.util.ValueIterator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-    public static final  int FUNC_LOGIN1 = 1;
-    public static final  int FUNC_LOGIN2 = 2;
-    public static final  int FUNC_LOGIN3 = 3;
-    public static final  int FUNC_LOGIN4 = 4;
-    public static final  int FUNC_LOGIN5 = 5;
-    public static final  int FUNC_LOGIN6 = 6;
-    public static final  int FUNC_LOGIN7 = 7;
-    public static final  int FUNC_LOGIN8 = 8;
+public static final int Func3 = 3;
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == FUNC_LOGIN1){
-            if (resultCode == RESULT_OK){
-
-            }else {
-                finish();
-            }
-        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b1 = (Button) findViewById(R.id.button1);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivityForResult(getIntent(),FUNC_LOGIN1);
+
+
+    }
+   public void func1(View view){
+        int func1 = 1;
+       Intent intent = new Intent(this,Func1Activity.class);
+       startActivityForResult(intent,func1);
+   }
+    public  void  func2(View view){
+       int func2 = 2;
+       Intent intent = new Intent(this,Func2Activity.class);
+       startActivityForResult(intent,func2);
+    }
+    public void  func3(View view){
+        int func3 = 3;
+        Intent intent = new Intent(this,Func3Activity.class);
+        startActivityForResult(intent,func3);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "特殊功能", Toast.LENGTH_LONG).show();
             }
-        });
+        }
+    public void func4(View view){
+        int func4 = 4;
+        Intent intent = new Intent(this,Func4Activity.class);
+        startActivityForResult(intent,func4);
+    }
+    public void func5(View view){
+        int func5 = 5;
+        Intent intent = new Intent(this,Func5Activity.class);
+        startActivityForResult(intent,func5);
+    }
+    public void func6(View view){
+        int func6 = 6;
+        Intent intent = new Intent(this,Func6Activity.class);
+        startActivityForResult(intent,func6);
+    }
+    public void func7(View view){
+        int func7 = 7;
+        Intent intent = new Intent(this,Func7Activity.class);
+        startActivityForResult(intent,func7);
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        if (resultCode == RESULT_OK){
+            Toast.makeText(this,"特殊功能",Toast.LENGTH_LONG).show();
+        }
+    }
+    public void func8(View view){
+        int func8 = 8;
+        Intent intent = new Intent(this,Func8Activity.class);
+        startActivityForResult(intent,func8);
     }
 }
+
 
